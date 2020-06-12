2d0433ccdae20d125db3d5cd3cbf28fc33b36de0
Matthieu Mac Nab <matthieu.macnab@h2ofintech.com>
2020-06-12 14:52:11 +0200

    🐛 fix(frontend): make it working

---
+++ frontend/list_of_movies/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ hello }}</div>
+</template>
+
+<script>
+
+import { HelloWorld } from './graphql/HelloWorld.gql'
+
+export default {
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    hello: {
+      query: HelloWorld
+    }
+  },
+}
+</script>
