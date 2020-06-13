81f0b1823fddf6fd181a1cbd08a62406dd2924db
jdelespinay <joseph.lespinay@gmail.com>
2020-06-13 11:21:00 +0200

    refactor : clean

--- frontend/list_of_movies/src/App.vue
+++ frontend/list_of_movies/src/App.vue
@@ -8,7 +8,6 @@ import { HelloWorld } from './graphql/HelloWorld.gql'
 
 export default {
   apollo: {
-    // Simple query that will update the 'hello' vue property
     hello: {
       query: HelloWorld
     }
