abfd5b4cc16fd9fc2ba04026050c4a810dd26b56
tos9lm <guillaume.rodier1992@gmail.com>
2020-06-16 18:02:23 +0200

    Just sort asc

--- frontend/list_of_movies/src/components/TabMovies.vue
+++ frontend/list_of_movies/src/components/TabMovies.vue
@@ -4,10 +4,11 @@
       <thead>
         <tr>
           <th v-for="column in this.columns"
-              v-bind:key="column.id"
-              @click="mySort(column); invertSort()">
+              v-bind:key="column"
+              :class="{ active : column == isActive }"
+              @click="mySort(column);">
             {{ column }}
-            <span class="arrow" :class="sortAsc > 0 ? 'asc' : 'dsc'">
+            <span class="arrow" :class="sortAsc == true && column == isActive ? 'dsc' : 'asc'">
             </span>
           </th>
         </tr>
@@ -32,36 +33,45 @@ export default {
   data() {
     return {
       columns: ["rank", "title", "synopsis", "duration", "director"],
-      sortAsc: true
+      sortAsc: false,
+      isActive: ""
     }
   },
   methods: {
     mySort(column) {
-      console.log(column);
       let ascDesc = this.sortAsc ? -1 : 1;
 
       switch (column) {
         case 'title':
           this.listMovies.sort((a, b) => ascDesc * a.title.localeCompare(b.title));
+          this.isActive = 'title';
           break;
         case 'synopsis':
           this.listMovies.sort((a, b) => ascDesc * a.synopsis.localeCompare(b.synopsis));
+          this.isActive = 'synopsis';
           break;
         case 'duration':
           this.listMovies.sort((a, b) => ascDesc * (a.duration === b.duration ? 0 : a.duration > b.duration ? 1 : -1));
+          this.isActive = 'duration';
           break;
         case 'director':
           this.listMovies.sort((a, b) => ascDesc * a.director.localeCompare(b.director));
+          this.isActive = 'director';
           break;
         case 'rank':
           this.listMovies.sort((a, b) => ascDesc * (a.rank === b.rank ? 0 : a.rank > b.rank ? 1 : -1));
+          this.isActive = 'rank';
           break;
         default:
           console.log(`Sorry, we are out of column.`);
       }
     },
-    invertSort() {
-      this.sortAsc = !this.sortAsc;
+    invertSort(trueColumn = false) {
+      if (trueColumn == false) {
+        this.sortAsc = !this.sortAsc;
+      } else {
+        this.sortAsc = false;
+      }
     }
   }
 }
