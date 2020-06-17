bbabc78da3197c232dd3dbe3375a37796ca78740
tos9lm <guillaume.rodier1992@gmail.com>
2020-06-17 15:26:37 +0200

    Sort table ok in graphical component

--- frontend/list_of_movies/src/components/TabMovies.vue
+++ frontend/list_of_movies/src/components/TabMovies.vue
@@ -8,7 +8,7 @@
               :class="{ active : column == isActive }"
               @click="mySort(column);">
             {{ column }}
-            <span class="arrow" :class="sortAsc == true && column == isActive ? 'dsc' : 'asc'">
+            <span class="arrow" :class="sortAsc == true ? 'dsc' : 'asc'">
             </span>
           </th>
         </tr>
@@ -33,45 +33,44 @@ export default {
   data() {
     return {
       columns: ["rank", "title", "synopsis", "duration", "director"],
-      sortAsc: false,
+      sortAsc: true,
       isActive: ""
     }
   },
   methods: {
     mySort(column) {
-      let ascDesc = this.sortAsc ? -1 : 1;
+      let ascDesc;
+
+      if (this.isActive != column) {
+        this.isActive = column;
+        ascDesc = this.sortAsc = true;
+      } else {
+        this.invertSort();
+        ascDesc = this.sortAsc ? 1 : -1;
+      }
 
       switch (column) {
         case 'title':
           this.listMovies.sort((a, b) => ascDesc * a.title.localeCompare(b.title));
-          this.isActive = 'title';
           break;
         case 'synopsis':
           this.listMovies.sort((a, b) => ascDesc * a.synopsis.localeCompare(b.synopsis));
-          this.isActive = 'synopsis';
           break;
         case 'duration':
           this.listMovies.sort((a, b) => ascDesc * (a.duration === b.duration ? 0 : a.duration > b.duration ? 1 : -1));
-          this.isActive = 'duration';
           break;
         case 'director':
           this.listMovies.sort((a, b) => ascDesc * a.director.localeCompare(b.director));
-          this.isActive = 'director';
           break;
         case 'rank':
           this.listMovies.sort((a, b) => ascDesc * (a.rank === b.rank ? 0 : a.rank > b.rank ? 1 : -1));
-          this.isActive = 'rank';
           break;
         default:
           console.log(`Sorry, we are out of column.`);
       }
     },
-    invertSort(trueColumn = false) {
-      if (trueColumn == false) {
-        this.sortAsc = !this.sortAsc;
-      } else {
-        this.sortAsc = false;
-      }
+    invertSort() {
+      this.sortAsc = !this.sortAsc;
     }
   }
 }
