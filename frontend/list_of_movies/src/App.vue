8fbfaed64abe0de362eb08f13c207bc386e2d2be
tos9lm <guillaume.rodier1992@gmail.com>
2020-06-19 11:51:47 +0200

    style

--- frontend/list_of_movies/src/App.vue
+++ frontend/list_of_movies/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <TabMovies :listMovies="movies" />
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand tag="h1" class="mb-0">Fullstack Technical Test</b-navbar-brand>
+    </b-navbar>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="10"><TabMovies :listMovies="movies" /></b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
