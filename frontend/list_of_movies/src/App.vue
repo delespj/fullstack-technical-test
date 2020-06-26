ceb8bc2cfb5a042a36af9f7bb5777cbcf503c26a
bbouvard <eliott.bouvard@gmail.com>
2020-06-26 18:22:36 +0200

    Added component MoviesTable and gql query Movies

--- frontend/list_of_movies/src/App.vue
+++ frontend/list_of_movies/src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <div>{{ hello }}</div>
+  <div>
+    <div>{{ hello }}</div>
+    <div>{{ movies }}</div>
+  </div>
 </template>
 
 <script>
-
-import { HelloWorld } from './graphql/HelloWorld.gql'
+import { HelloWorld } from "./graphql/HelloWorld.gql";
+import { Movies } from "./graphql/Movies.gql";
 
 export default {
   apollo: {
     hello: {
       query: HelloWorld
+    },
+    movies: {
+      query: Movies
     }
-  },
-}
+  }
+};
 </script>
