c34f405706a639093ec7e5d4b926e45fba0a7c2f
bbouvard <eliott.bouvard@gmail.com>
2020-06-26 18:23:37 +0200

    Deleted ref to HelloWorld query in App.vue

--- frontend/list_of_movies/src/App.vue
+++ frontend/list_of_movies/src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
-    <div>{{ hello }}</div>
     <div>{{ movies }}</div>
   </div>
 </template>
 
 <script>
-import { HelloWorld } from "./graphql/HelloWorld.gql";
 import { Movies } from "./graphql/Movies.gql";
 
 export default {
   apollo: {
-    hello: {
-      query: HelloWorld
-    },
     movies: {
       query: Movies
     }
