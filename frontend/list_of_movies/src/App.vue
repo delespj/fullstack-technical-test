d19b38cba634bf08070118effeec8a3334da22b1
Jean-Paul CHAMPY <champy_jean-paul@live.fr>
2020-06-27 17:48:01 +0200

    reading movie from db instead of helloworld

--- frontend/list_of_movies/src/App.vue
+++ frontend/list_of_movies/src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <div>{{ hello }}</div>
+  <div>{{ movies }}</div>
 </template>
 
 <script>
-
+//{{ hello }}
 import { HelloWorld } from './graphql/HelloWorld.gql'
+import { Movies } from './graphql/Movies.gql'
 
 export default {
   apollo: {
     hello: {
       query: HelloWorld
-    }
+    },
+	movies:{
+		query: Movies
+	}
   },
 }
 </script>
