172b28e58a993c88fdb4e21dd23ac2973d39d7ac
Jean-Paul CHAMPY <champy_jean-paul@live.fr>
2020-06-28 22:07:45 +0200

    sorting on frontend using Vue Material

--- frontend/list_of_movies/src/App.vue
+++ frontend/list_of_movies/src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <div>{{ movies }}</div>
+  <div>
+    <md-table v-model="movies" md-sort="rank" md-sort-order="asc" md-card>
+		<md-table-row slot="md-table-row" slot-scope="{ item }">
+			<md-table-cell md-label="Rank" md-sort-by="rank" md-numeric>{{ item.rank }}</md-table-cell>
+			<md-table-cell md-label="Title" md-sort-by="title">{{ item.title }}</md-table-cell>
+			<md-table-cell md-label="Synopsis" md-sort-by="synopsis">{{ item.synopsis }}</md-table-cell>
+			<md-table-cell md-label="Director" md-sort-by="director">{{ item.director }}</md-table-cell>
+			<md-table-cell md-label="Duration" md-sort-by="duration">{{ item.duration }}</md-table-cell>
+		</md-table-row>
+    </md-table>
+  </div>
 </template>
 
-<script>
-//{{ hello }}
-import { HelloWorld } from './graphql/HelloWorld.gql'
-import { Movies } from './graphql/Movies.gql'
+ <script>
+
+import {
+	Movies
+}
+from './graphql/Movies.gql'
 
 export default {
-  apollo: {
-    hello: {
-      query: HelloWorld
-    },
-	movies:{
-		query: Movies
-	}
-  },
+	apollo: {
+		movies: {
+			query: Movies
+		}
+	},
 }
+//movies[0].id  for the id only
 </script>
--- frontend/list_of_movies/src/main.js
+++ frontend/list_of_movies/src/main.js
@@ -4,7 +4,14 @@ import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
 new Vue({
   apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
+
